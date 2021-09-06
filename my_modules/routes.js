d4dbe3eda6dbb33b713aac88e058a1db16b17d7a
Limsa00 <bennis.salim@gmail.com>
2021-09-06 16:31:25 +0200

    config of moment tz for each city

--- my_modules/routes.js
+++ my_modules/routes.js
@@ -2,6 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 const capitalCities = require('./capitalCities');
+var moment = require('moment-timezone');
+
+moment.locale('fr');
 
 router.get('/', (req, res) => {
     res.sendFile('index.html', {
@@ -15,13 +18,15 @@ router.get('/city/:slug', (req,res) => {
 
     for (let index = 0; index < capitalCities.length; index++) {
         const city = capitalCities[index];
-
-        // console.log('cityName : ', cityName);
-        // console.log('city.name.toLowerCase() : ', city.name.toLowerCase());
-        
+      
         if(cityName === city.name.toLowerCase()){
             res.type('html');
-            res.send(`<h1>Heure de ${city.name}</h1>`)
+            const now = moment();
+            // console.log('now : ', now)
+            const nowTz = now.tz(city.tz);
+            // console.log('nowTz : ', nowTz);
+            const formattedDate = nowTz.format('dddd Do MMMM YYYY, h:mm:ss a');
+            res.send(`<h1>A ${city.name}, il est ${formattedDate}</h1>`);
             return
         }
     }
